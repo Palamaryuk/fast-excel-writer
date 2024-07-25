294adf29255ff78ac6fa5dedf0f40681b5fd8c96
Alexandr Palamaryuk <a.palamaryuk@domyland.ru>
2024-07-25 18:10:15 +0500

    Revert "change composer.json"
    This reverts commit 35bc2d57244c9bf47a594fe4f5ad91b544ade768.

--- src/autoload.php
+++ src/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(static function ($class) {
-    $namespace = 'palamaryuk\\FastExcelWriter\\';
+    $namespace = 'avadim\\FastExcelWriter\\';
     if (0 === strpos($class, $namespace)) {
         include __DIR__ . '/FastExcelWriter/' . str_replace($namespace, '', $class) . '.php';
     }
